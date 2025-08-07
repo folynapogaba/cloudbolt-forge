74bc3b8339682869f0e16119c1d6ead50e824a4c
folynapogaba <111736187+folynapogaba@users.noreply.github.com>
2025-08-07 10:08:57 -0400

    Update main.tf
    remove terraform provider

--- how_do_i_videos/use_terraform_operations/terraform/s3_website/main.tf
+++ how_do_i_videos/use_terraform_operations/terraform/s3_website/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.8.0"
-    }
-  }
-
-  required_version = ">= 1.1.7"
-}
 
 # Configure the AWS Provider
 provider "aws" {
